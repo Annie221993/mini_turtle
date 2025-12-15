9e2fc7c2672b552076fff67a13e70b8263d89ee0
Annie221993 <chrislider1993@gmail.com>
2025-12-14 19:15:33 -0500

    Update _init_.py

--- mini_turtle_oo/mini_turtle_oo/_init_.py
+++ mini_turtle_oo/mini_turtle_oo/_init_.py
@@ -1,10 +1,1 @@
-class Tortuga:
-    def _init_(self):
-        self.posicion_x = 0
 
-    def adelante(self, pasos):
-        self.posicion_x += pasos
-        print(f"Tortuga avanza {pasos} pasos. Posición actual: {self.posicion_x}")
-
-    def abajo(self):
-        print("Tortuga baja el lápiz")

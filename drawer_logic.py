b4ea01efca4b7e3752a1435b13611011b8d89c2a
Annie221993 <chrislider1993@gmail.com>
2025-12-14 19:12:18 -0500

    Update drawer_logic.py

--- drawer_logic.py
+++ drawer_logic.py
@@ -1,13 +1,1 @@
-posicion_x = 0
 
-def adelante(pasos):
-    global posicion_x
-    posicion_x = posicion_x + pasos
-    print(posicion_x)
-
-def abajo(pasos):
-    print("abajo")
-
-def reiniciar():
-    global posicion_x
-    posicion_x = 0

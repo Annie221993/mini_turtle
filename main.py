461d364c138bb4b735fec1722e67e55dfbc6219a
Annie221993 <chrislider1993@gmail.com>
2025-12-14 19:17:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from mini_turtle_oo import Tortuga
 
-print("Prueba con dos tortugas independientes")
-
-t1 = Tortuga()
-t2 = Tortuga()
-
-t1.adelante(5)
-t2.adelante(10)
-
-t1.adelante(3)
-t2.adelante(2)

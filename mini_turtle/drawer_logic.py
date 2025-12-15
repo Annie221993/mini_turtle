595ef8422ecec09fc5b16f7be390c917902e93ad
Annie221993 <chrislider1993@gmail.com>
2025-12-14 19:36:39 -0500

    Update drawer_logic.py

--- mini_turtle/drawer_logic.py
+++ mini_turtle/drawer_logic.py
@@ -1,17 +1,1 @@
-# drawer_logic.py
-# Aquí va la lógica de dibujo
 
-posicion_x = 0
-
-def adelante(pasos=50):
-    global posicion_x
-    posicion_x += pasos
-    print(f"Tortuga avanza a la posición {posicion_x}")
-
-def abajo():
-    print("Tortuga baja una línea")
-
-def reiniciar():
-    global posicion_x
-    posicion_x = 0
-    print("Posición reiniciada a 0")

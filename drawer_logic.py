3bd593d3276eea6209ae1fb93b170d4b896d8df7
Annie221993 <chrislider1993@gmail.com>
2025-12-14 19:49:46 -0500

    Update drawer_logic.py

--- drawer_logic.py
+++ drawer_logic.py
@@ -1,17 +1,1 @@
-# mini_turtle/drawer_logic.py
 
-# Variable global que guarda la posición
-posicion_x = 0
-
-def adelante(pasos):
-    global posicion_x
-    posicion_x += pasos
-    print(f"Avanzando {pasos} pasos → Posición actual: {posicion_x}")
-
-def abajo():
-    print("Bajando una línea")
-
-def reiniciar():
-    global posicion_x
-    posicion_x = 0
-    print("Posición reiniciada a 0")
